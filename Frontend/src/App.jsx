29e618b5e8f816667d75af3105204280effc000d
Vince-arch <vincekimani006@gmail.com>
2025-02-06 14:00:57 +0300

    adding navbar routes

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -3,15 +3,21 @@ import "../src/index.css"
 import { BrowserRouter as Router } from 'react-router-dom';
 import {Route, Routes} from 'react-router-dom'
 import Home from './pages/Home'
+import  Auth  from './pages/Auth';
+import  Community  from './pages/Community';
+import Profile  from './pages/Profile';
+import Workouts from './pages/Workouts';
 
 function App() {
 
   return (
     <Router>   
         <Routes>
-          <Route path="/" element={ <Home />}>
-           
-          </Route>
+          <Route path="/" element={ <Home />}/>,
+          <Route path='auth' element={<Auth />}/>,
+          <Route path="community" element={<Community/>}/>
+          <Route path="profile" element={<Profile/>}/>
+          <Route path="workouts" element={<Workouts/>}/>
         </Routes>
     </Router>
   )
