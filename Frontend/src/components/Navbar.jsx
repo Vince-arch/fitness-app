3baca6717a8d53953189b6d086d98db551afbe14
Vince-arch <vincekimani006@gmail.com>
2025-02-06 13:23:51 +0300

    adding links

--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function Navbar() {
   return (
+    <nav className="p-4 bg-gray-800 text-white flex justify-between">
+      <h1 className="text-xl font-bold">Fitness</h1>
     <div>
-        <div className='bg-blue-400 h-24'>
-            navbar
-        </div>
+        <Link to="/" className='bg-blue-400 mr-4'>Home</Link>
     </div>
+    </nav>
   )
 }
 
